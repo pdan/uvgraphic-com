d6fabfb092850edec05a3ed2fb7a353ddd105dd6
Pourya Daneshvar <pourya@daneshvar.me>
2015-09-24 09:41:46 +0330

    Improve gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,153 +2,147 @@
 
 module.exports = function(grunt) {
 
-	// Load grunt tasks automatically
-	require('load-grunt-tasks')(grunt, {
-		pattern: [ // '@*/grunt-*', 'assemble-less',
-			'grunt-*'
-		]
-	});
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt, {
+    pattern: [ // '@*/grunt-*', 'assemble-less',
+      'grunt-*'
+    ]
+  });
 
-	// Define the configuration for all the tasks
-	grunt.initConfig({
-		// Project settings
-		app: {
-			public: 'public',
-			bower: require('./.bowerrc').directory || 'bower_components'
-		},
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+    // Project settings
+    app: {
+      public: 'public',
+      bower: require('./.bowerrc').directory || 'bower_components'
+    },
 
-		// Watches files for changes and runs tasks based on the changed files
-		watch: {
-			less: {
-				files: ['<%= app.public %>/less/{,*/}*.less'],
-				tasks: ['less:dev']
-			}
-		},
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      less: {
+        files: ['<%= app.public %>/less/{,*/}*.less'],
+        tasks: ['less:dev']
+      }
+    },
 
-		// Copies remaining files to places other tasks can use
-		copy: {
-			dist: {
-				files: [{
-					expand: true,
-					dot: true,
-					flatten: true,
-					cwd: '<%= doc.bower %>',
-					dest: '<%= doc.static %>/scripts/vendor',
-					src: [
-						'jquery/dist/jquery.js',
-						'bootstrap/dist/js/bootstrap.js'
-					]
-				}, {
-					expand: true,
-					flatten: true,
-					cwd: '<%= doc.bower %>',
-					dest: '<%= doc.static %>/fonts/vendor',
-					src: ['ionicons/fonts/*', 'font-awesome/fonts/fontawesome-*']
-				}]
-			}
-		},
+    // Compile less styles to app
+    less: {
+      dev: {
+        files: {
+          '<%= app.public %>/css/main.css': [
+            '<%= app.public %>/less/main.less'
+          ],
+          '<%= app.public %>/css/admin.css': [
+            '<%= app.public %>/less/admin.less'
+          ]
+        }
+      },
+      product: {
+        options: {
+          compress: true
+        },
+        files: {
+          '<%= doc.static %>/styles/main.css': [
+            '<%= doc.render %>/less/main.less'
+          ]
+        }
+      }
+    },
 
-		// Compile less styles to app
-		less: {
-			dev: {
-				files: {
-					'<%= app.public %>/css/main.css': [
-						'<%= app.public %>/less/main.less'
-					],
-					'<%= app.public %>/css/admin.css': [
-						'<%= app.public %>/less/admin.less'
-					]
-				}
-			},
-			product: {
-				options: {
-					compress: true
-				},
-				files: {
-					'<%= doc.static %>/styles/main.css': [
-						'<%= doc.render %>/less/main.less'
-					]
-				}
-			}
-		},
+    uglify: {
+      dist: {
+        files: {
+          '<%= doc.static %>/scripts/scripts.min.js': [
+            '<%= doc.static %>/scripts/scripts.js'
+          ]
+        }
+      }
+    },
 
-		uglify: {
-			dist: {
-				files: {
-					'<%= doc.static %>/scripts/scripts.min.js': [
-						'<%= doc.static %>/scripts/scripts.js'
-					]
-				}
-			}
-		},
+    filerev: {
+      options: {
+        algorithm: 'md5',
+        length: 8
+      },
+      js: {
+        src: 'build.js',
+        dest: 'public/js/main'
+      }
+    },
 
-		concat: {
-			dist: {
-				src: [
-					'<%= doc.static %>/scripts/vendor/jquery.js',
-					'<%= doc.static %>/scripts/vendor/bootstrap.js',
-					'<%= doc.render %>/js/functions.js',
-					'<%= doc.render %>/js/script.js',
-					'<%= doc.render %>/js/materialoe/*.js'
-				],
-				dest: '<%= doc.static %>/scripts/scripts.js'
-			}
-		},
+    shell: {
+      options: {
+        stderr: false
+      },
+      target: {
+        command: 'jspm bundle-sfx js/main/index.js public/js/main/build.js'
+      }
+    },
 
-		imagemin: {
-			dist: {
-				files: [{
-					expand: true,
-					cwd: '<%= doc.static %>/images',
-					src: '{,*/}*.{png,jpg,jpeg,gif}',
-					dest: '<%= doc.static %>/images'
-				}]
-			}
-		},
+    htmlbuild: {
+      dist: {
+        src: 'views/index.html',
+        dest: 'views/build/',
+        options: {
+          beautify: true,
+          // prefix: '//some-cdn',
+          // relative: true,
+          scripts: {
+            build: '<%= app.public %>/js/main/build.*.js'
+          },
+        }
+      }
+    },
+    clean: {
+      first: ['<%= app.public %>/js/main/build.*.js'],
+			last: ['<%= app.public %>/js/main/build.js']
+    },
 
-		svgmin: {
-			dist: {
-				files: [{
-					expand: true,
-					cwd: '<%= doc.static %>/images',
-					src: '{,*/}*.svg',
-					dest: '<%= doc.static %>/images'
-				}]
-			}
-		}
-	});
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= doc.static %>/images',
+          src: '{,*/}*.{png,jpg,jpeg,gif}',
+          dest: '<%= doc.static %>/images'
+        }]
+      }
+    },
 
-	grunt.registerTask('serve', 'Compile then start a connect web server',
-		function(target) {
-			if (target === 'dist') {
-				// return grunt.task.run(['build', 'connect:dist:keepalive']);
-				return;
-			}
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= doc.static %>/images',
+          src: '{,*/}*.svg',
+          dest: '<%= doc.static %>/images'
+        }]
+      }
+    }
+  });
 
-			grunt.task.run([
-				'watch'
-			]);
-		});
+  grunt.registerTask('serve', 'Compile then start a connect web server',
+    function(target) {
+      if (target === 'dist') {
+        // return grunt.task.run(['build', 'connect:dist:keepalive']);
+        return;
+      }
 
-	grunt.registerTask('init', 'Prepare project for development',
-		function() {
-			grunt.task.run([
-				'less:dev',
-				'copy'
-			]);
-		});
+      grunt.task.run([
+        'watch'
+      ]);
+    });
 
-	grunt.registerTask('build', 'Build product',
-		function() {
-			grunt.task.run([
-				'less:product',
-				'copy',
-				'concat',
-				'uglify',
-				'imagemin',
-				'svgmin'
-			]);
-		});
+  grunt.registerTask('build', 'Build product',
+    function() {
+      grunt.task.run([
+        'clean:first',
+        'shell:jspm',
+        'filerev:js',
+        'htmlbuild',
+				'clean:last'
+      ]);
+    });
 
-	grunt.registerTask('default', []);
+  grunt.registerTask('default', []);
 };
